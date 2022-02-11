271b1cb74931ac5a4a3fe8b4bac90b210def8393
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-02-11 14:13:20 +0530

    new db relation

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -35,8 +35,7 @@ ActiveAdmin.register_page "Dashboard" do
       urls.append(params[:start_test][:text].split(' ')) 
     end
     test = Test.create(number_of_urls: urls.size,status:0)
-    chunks = (urls.size+50)/50
-    urls.in_groups_of(10){ |_urls|
+    urls.in_groups_of(50){ |_urls|
         BlogvaultScrapingJob.perform_later(_urls,test.id)
     }
   end
