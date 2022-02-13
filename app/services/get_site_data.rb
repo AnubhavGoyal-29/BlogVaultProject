cbce8280177868befba98378a34000ee347e8b57
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-02-13 11:38:20 +0530

    code quality has been improved a little more

--- app/services/get_site_data.rb
+++ app/services/get_site_data.rb
@@ -2,24 +2,41 @@ class GetSiteData
   class<<self
     def start_scraping_html(urls_data)
       data = []
-      for i in 0..urls_data[1].size-1 do
-        html =urls_data[1][i]
-        scripts = html.css('script')
-        barr = Hash.new{|h,k| h[k] = [] }
-        scripts.each do |script|
-          if script['src'] and script['src']['wp-content']
-            arr = script['src'].split('/')
-            arr = arr.reverse
-            j = arr.index('wp-content')
-            if arr[j-2]
-              barr[arr[j-1]].push(arr[j-2])
-            end
-          end
-        end 
-        puts barr
-        data << [urls_data[0][i],barr,urls_data[2][i]]
+      for index in 0..urls_data[1].size-1 do
+        html = urls_data[1][index]
+        hashedData = Hash.new{|h,k| h[k] = []}
+
+        get_data_from_resource(html,'script','plugins',hashedData)
+        get_data_from_resource(html,'script','themes',hashedData)
+        get_data_from_resource(html,'script','js',hashedData)
+        get_data_from_resource(html,'link','js',hashedData)
+
+        data << [urls_data[0][index],hashedData,urls_data[2][index]]
       end
       return data
     end
+
+
+
+    def get_data_from_resource(html,resource,dataType,hashedData)
+      resource_data = html.css(resource)
+      resource_data.each do |line|
+        get_data_from_sub_source(line,dataType,'src',hashedData)
+        get_data_from_sub_source(line,dataType,'href',hashedData)
+      end
+    end
+
+    def get_data_from_sub_source(line,dataType,subResource,hashedData)
+      if line[subResource] and line[subResource][dataType]
+        tempArr = line[subResource].split('/')          #tempArr stores string values spllitted by '/' sign in order to obtain resource and its next value
+        tempArr = tempArr.reverse
+        dataTypeIndex = tempArr.index(dataType)
+        if dataTypeIndex and tempArr[dataTypeIndex-1]
+          hashedData[dataType].push(tempArr[dataTypeIndex-1])
+        end
+      end
+    end
+
+
   end
 end
