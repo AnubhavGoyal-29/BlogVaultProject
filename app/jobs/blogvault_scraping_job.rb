2ce2aa91f8f165c1adc75c435f97c6ea26e0044f
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-02-16 18:08:29 +0530

    final commit to this branch

--- app/jobs/blogvault_scraping_job.rb
+++ app/jobs/blogvault_scraping_job.rb
@@ -5,11 +5,13 @@ class BlogvaultScrapingJob < ApplicationJob
     @logger ||= Logger.new("log/testing.log")
   end
 
-  def perform(urls, test)
-    logger.info "Blogvault Scraping Job Started"
+  def perform(urls, test, id)
+    logger.info "Blogvault Scraping Job #{id} Started"
     data = Scrape::filter_wp_urls(urls, logger)             # here data will be maped agains url id from our database tables
     data = Scrape::scrape_html(data, logger)
-    SiteDataInfo.import_data(test, data)
+    logger.info "scraping completed for #{id}"
+    SiteDataInfo.import_data(test, data, logger, id)
+    logger.info "database update completed for #{id} "
     if Resque.redis.lrange("queue:#{'default'}",0,-1).count == 0 && Resque.info[:working] == 1
         test = Test.find(test)
         test.status = 1
