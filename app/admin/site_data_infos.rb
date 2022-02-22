6d091a876a97a329c37af992314ecc7594581312
sujay-00 <sujaysingh1999.sis@gmail.com>
2022-02-22 12:18:34 +0530

    fixed UI

--- app/admin/site_data_infos.rb
+++ app/admin/site_data_infos.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register SiteDataInfo do
   filter :test_id
   filter :url_id
   filter :cloudflare, :as => :select, :collection => SiteDataInfo::STATUS.invert
-  
+
 
   index do 
     column "Test" do |site_data|
@@ -18,14 +18,34 @@ ActiveAdmin.register SiteDataInfo do
     column 'Versions' do |site_data|
       link_to 'versions', admin_site_data_infos_path('q[url_id_equals]' => site_data.url_id)
     end
-    column "Plugins" do |site|
-      link_to 'plugins', admin_plugins_path("q[url_id_equals]" => site.url_id, "q[status_equals]" => 1)
+    column 'Plugins' do |site_data|
+      plugins = site_data.plugins
+      if JSON::parse(plugins).size > 0
+        link_to 'plugins', admin_plugins_path("q[url_id_equals]" => site_data.url_id, "q[status_equals]" => 1)
+      else
+        div("plugin not found", style: "color: red")
+      end
     end
+    column 'Themes' do |site_data|
+      themes = site_data.themes
+      if JSON::parse(themes).size > 0
+        link_to 'Themes', admin_themes_path("q[url_id_equals]" => site_data.url_id, "q[status_equals]" => 1)
+      else
+        div("Theme not found", style: "color: red")
+      end
+    end
+
+    column 'Js' do |site_data|
+      js = site_data.js
+      if JSON::parse(js).size > 0
+        link_to 'Js', admin_js_infos_path("q[url_id_equals]" => site_data.url_id, "q[status_equals]" => 1)
+      else
+        div("js not found", style: "color: red")
+      end
+    end
+
     column "Js" do |site|
       link_to 'js', admin_js_infos_path("q[url_id_equals]" => site.url_id, "q[status_equals]" => 1)
-    end
-    column "Themes" do |site|
-      link_to 'themes', admin_themes_path("q[url_id_equals]" => site.url_id, "q[status_equals]" => 1)
     end
     column 'Cloudflare' do |site|
       if site.cloudflare == '0'
@@ -46,8 +66,8 @@ ActiveAdmin.register SiteDataInfo do
       row :cms_type
       row :cms_version
       row 'Versions' do |site_data|
-      link_to 'versions', admin_site_data_infos_path('q[url_id_equals]' => site_data.url_id)
-    end
+        link_to 'versions', admin_site_data_infos_path('q[url_id_equals]' => site_data.url_id)
+      end
       row "Plugins" do |site|
         link_to 'plugins', admin_plugins_path("q[url_id_equals]" => site.url_id, "q[status_equals]" => 1)
       end
