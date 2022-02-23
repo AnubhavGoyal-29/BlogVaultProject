d903e2ae0c96b683c48b2a169c2f6ea0cb8faeae
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-02-23 15:42:21 +0530

    bla

--- app/admin/tests.rb
+++ app/admin/tests.rb
@@ -9,6 +9,9 @@ ActiveAdmin.register Test do
       url_ids = SiteDataInfo.where(test_id: 2).pluck(:url_id).to_s
       link_to 'data info', admin_site_data_infos_path("q[test_id_equals]" => test.id )
     end
+    column 'Steps' do |test|
+      link_to 'steps', admin_steps_path('q[test_id_equals]' => test.id)
+    end
     column 'Status' do |test|
       status = test.status 
       options = Test::STATUS.invert
