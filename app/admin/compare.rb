cd48f671f69eb92d32026f53aa5eb5d50a65e49a
sujay-00 <sujaysingh1999.sis@gmail.com>
2022-03-01 17:56:23 +0530

    commit for pull

--- app/admin/compare.rb
+++ app/admin/compare.rb
@@ -5,10 +5,11 @@ ActiveAdmin.register_page "Compare" do
     if params[:_method] == "get"
       flash[:notice] = params
       #redirect_to admin_compare_path
-      render :partial => "form", :locals => { :id => params[:url_id] }
+      render :partial => "form", :locals => { :url_id => params[:url_id] }
     else
-      #flash[:notice] = params
-      
+      flash[:notice] = params
+      render :partial => "table"
+      redirect_to admin_compare_path
     end
   end
 end 
