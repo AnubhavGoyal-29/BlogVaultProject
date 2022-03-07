5b864bf13ba2f21124a6a71aa10159b75a791bb2
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-03-07 12:35:20 +0530

    for code review

--- db/migrate/20220210074613_create_js_info.rb
+++ db/migrate/20220210074613_create_js_info.rb
@@ -3,7 +3,6 @@ class CreateJsInfo < ActiveRecord::Migration[5.2]
     create_table :js_infos do |t|
       t.string :js_name
       t.string :url_id
-      t.string :test_id
       t.integer :status
       t.string :version
       t.string :other_data
--- db/migrate/20220305050801_add_column_started_at_in_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnStartedAtInTests < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tests, :started_at, :timestamp
-  end
-end
