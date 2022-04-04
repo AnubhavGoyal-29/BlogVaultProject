663d46d7f451f11319ce0e6fc7ce2b691205d89b
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-04-04 17:34:01 +0530

    history code done

--- app/models/v2/test.rb
+++ app/models/v2/test.rb
@@ -22,9 +22,17 @@ class V2::Test
     STATUS[Status.class_eval(type.to_s)] = type
   }
 
-  def self.history_of_tests
-    key -> test numbers 
-    hashes = [{:name => 'total sites', :data => {}},{:name => 'wordpress sites', :data => }]
-  end
+  def self.history_of_tests(time1, time2)
+    total_urls = {}
+    wordpress_urls = {}
+    V2::Test.where({:created_at.gte => time1, :created_at.lte => time2}).each do |test|
+      total_urls[test.number] = test.number_of_websites
+      wordpress_urls[test.number] = V2::SiteDataInfo.where(:test => test).count
+    end
 
+    data = []
+    data << {:name => "Total Websites", :data => total_urls}
+    data << {:name => "Wordpress Sites", :data => wordpress_urls}
+    return data
+  end
 end
