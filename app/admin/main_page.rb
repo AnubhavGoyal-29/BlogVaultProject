bf4bef6901d4c0a72323adf46f4548b127a16a51
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-04-05 14:17:49 +0530

    adding form for history

--- app/admin/main_page.rb
+++ app/admin/main_page.rb
@@ -1,13 +1,3 @@
 ActiveAdmin.register_page "main_page" do
-  content do
-    text_node javascript_include_tag 'https://www.gstatic.com/charts/loader.js', skip_pipeline: true
-    panel "View Page" do
-      render :partial => "form"
-    end
-  end
-
-  page_action :history, :method => [:post, :get] do
-    flash[:notice] = params
-    redirect_to admin_main_page_path
-  end
+  page_action :history, :method => [:post, :get]
 end
