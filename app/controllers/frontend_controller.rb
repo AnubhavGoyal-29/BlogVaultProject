15eecdbd77ad172fbec99643082467797c32fb96
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-04-12 17:14:45 +0530

    cms_distribution added

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,11 +1,21 @@
 class FrontendController < ApplicationController
-  def show
+
+  def site_timeline
     if !params["history"]
       redirect_to admin_dashboard_path
     end
   end
+
+  def cms_distribution
+    if params[:cms_distribution].present?
+      if params[:cms_distribution][:start_date] > params[:cms_distribution][:end_date]
+        flash[:notice] = "please select date in correct order"
+      end
+    end
+  end
+
+  def source_timeline
+  end
+
 end
-~                                                                                                                                                      
-~                                                                                                                                                      
-~                                                                                                                                                      
-~                                              
+
